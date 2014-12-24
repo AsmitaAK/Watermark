74afa746e6299a241fefafc2fa6467a9d4bdd393
AsmitaAK <asmita.koli@gmail.com>
2014-12-24 13:11:38 +0530

    updated .js file

--- www/WatermarkDetector.js
+++ www/WatermarkDetector.js
@@ -1,11 +1,12 @@
 
 	
-	var exec ;
+	var exec 	var exec = require('cordova/exec') ; 
+	module.exports = {
     start: function(successCallback, errorCallback) {
         //exec(successCallback, errorCallback, "ContactVcardPicker", "getContactVcard", []);
         exec(successCallback, errorCallback, 'WatermarkDetector', 'start', []);
     }
-
+	};
 function success(data)
 {
 alert("success"+data); 
